54cdbe74ac072c4779c5468cc971014585649603
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-16 19:33:53 +0000

    add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import footer from '../Footer/Footer.css'
+
+const Footer = () => {
+    return (
+        <div class="footer">
+        <div class="container">
+          <div class="row">
+            <p class="text-center">
+             2021 Pizzamania All Rights reserved
+            </p>
+          </div>
+        </div>
+      </div>    );
+}
+
+export default Footer;
